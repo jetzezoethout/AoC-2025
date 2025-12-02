e37943a9f8d47eb761dd90ba6d9ae52fc87112d8
Jetze Zoethout <jzoethout@sogyo.nl>
2025-12-02 20:48:16 +0100

    Day 02 runs instantly

--- day02/IdRange.hs
+++ day02/IdRange.hs
@@ -6,30 +6,54 @@ import           Parsers   (parseInt)
 
 type ProductId = Int
 
-isSilly :: ProductId -> Bool
-isSilly = (`isRepeating` 2)
-
-isExtendedSilly :: ProductId -> Bool
-isExtendedSilly productId = any (productId `isRepeating`) [2 .. idLength]
-  where
-    idLength = length $ show productId
-
-isRepeating :: ProductId -> Int -> Bool
-productId `isRepeating` times =
-  concat (replicate times $ take (idLength `div` times) idString) == idString
-  where
-    idString = show productId
-    idLength = length idString
-
 data IdRange = IdRange
   { start :: ProductId
   , end   :: ProductId
   } deriving (Show)
+
+maxIdLength :: IdRange -> Int
+maxIdLength IdRange {..} = length $ show end
 
 parseIdRange :: Text -> IdRange
 parseIdRange text =
   let parts = T.splitOn "-" text
    in IdRange {start = parseInt $ head parts, end = parseInt $ parts !! 1}
 
-productIdsInRange :: IdRange -> [ProductId]
-productIdsInRange IdRange {..} = [start .. end]
+sumSillyPatterns :: IdRange -> Int -> Int -> Int
+sumSillyPatterns IdRange {..} patternRepeats patternLength =
+  if firstPattern > lastPattern
+    then 0
+    else ((firstPattern + lastPattern)
+            * (lastPattern - firstPattern + 1)
+            * factor)
+           `div` 2
+  where
+    factor = sum $ map ((10 ^) . (patternLength *)) [0 .. patternRepeats - 1]
+    firstPattern = max (10 ^ (patternLength - 1)) $ start `ceilDiv` factor
+    lastPattern = min (10 ^ patternLength - 1) $ end `div` factor
+
+ceilDiv :: Integral a => a -> a -> a
+x `ceilDiv` y = -((-x) `div` y)
+
+sumInvalid :: IdRange -> Int
+sumInvalid idRange =
+  sum $ map (sumSillyPatterns idRange 2) [1 .. maxIdLength idRange `div` 2]
+
+sumExtendedInvalid :: IdRange -> Int
+sumExtendedInvalid idRange =
+  sum
+    [ -(mu patternRepeats
+          * sumSillyPatterns idRange patternRepeats patternLength)
+    | patternRepeats <- [2 .. maxIdLength idRange]
+    , patternLength <- [1 .. maxIdLength idRange `div` patternRepeats]
+    ]
+
+mu :: Int -> Int
+mu 1 = 1
+mu n =
+  if largestDivisor `mod` smallestDivisor == 0
+    then 0
+    else negate $ mu largestDivisor
+  where
+    smallestDivisor = head $ filter ((== 0) . (n `mod`)) [2 ..]
+    largestDivisor = n `div` smallestDivisor
--- day02/Main.hs
+++ day02/Main.hs
@@ -1,16 +1,12 @@
 module Main where
 
 import qualified Data.Text   as T
-import           IdRange     (isExtendedSilly, isSilly, parseIdRange,
-                              productIdsInRange)
+import           IdRange     (parseIdRange, sumExtendedInvalid, sumInvalid)
 import           ProcessFile (processFile)
 
 main :: IO ()
 main =
   processFile $ \text -> do
     let idRanges = map parseIdRange $ T.splitOn "," text
-        productIds = idRanges >>= productIdsInRange
-        sillyProductIds = filter isSilly productIds
-        extendedSillyProductIds = filter isExtendedSilly productIds
-    print $ sum sillyProductIds
-    print $ sum extendedSillyProductIds
+    print $ sum $ map sumInvalid idRanges
+    print $ sum $ map sumExtendedInvalid idRanges
