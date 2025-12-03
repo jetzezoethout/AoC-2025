939118f7ee2df30fc3e04dd30d9755e1daec07b2
Jetze Zoethout <jzoethout@sogyo.nl>
2025-12-03 20:31:13 +0100

    Day 03 more readable and also faster

--- day03/BatteryBank.hs
+++ day03/BatteryBank.hs
@@ -18,19 +18,29 @@ maxJoltage batteriesNeeded batteryBank = foldl' (\x y -> 10 * x + y) 0 $ selectB
 selectBatteries :: Int -> BatteryBank -> [Battery]
 selectBatteries 0 _ = []
 selectBatteries batteriesNeeded batteryBank =
-  let (first, remaining) = selectFirstBattery batteriesNeeded batteryBank
-   in first : selectBatteries (batteriesNeeded - 1) remaining
+  let indexedFirst = selectFirstBattery batteriesNeeded batteryBank
+   in value indexedFirst : selectBatteries (batteriesNeeded - 1) (drop (index indexedFirst + 1) batteryBank)
 
-selectFirstBattery :: Int -> BatteryBank -> (Battery, BatteryBank)
-selectFirstBattery _ [] = error "bank is empty"
-selectFirstBattery batteriesNeeded (x:xs) = go x xs xs
+selectFirstBattery :: Int -> BatteryBank -> Indexed Battery
+selectFirstBattery batteriesNeeded xs =
+  case enumerate xs of
+    []         -> error "bank is empty"
+    first:rest -> go first first rest
   where
-    go maxSoFar tailSoFar ys
-      | length ys <= batteriesNeeded - 1 = (maxSoFar, tailSoFar)
-      | otherwise =
-        case ys of
-          [] -> (maxSoFar, tailSoFar)
-          (next:rest) ->
-            if next > maxSoFar
-              then go next rest rest
-              else go maxSoFar tailSoFar rest
+    totalLength = length xs
+    go maxSoFar current remaining =
+      if index current + batteriesNeeded >= totalLength
+        then maxSoFar
+        else let next = head remaining
+                 stillRemaining = tail remaining
+              in if value next > value maxSoFar
+                   then go next next stillRemaining
+                   else go maxSoFar next stillRemaining
+
+data Indexed a = Indexed
+  { index :: Int
+  , value :: a
+  }
+
+enumerate :: [a] -> [Indexed a]
+enumerate = zipWith Indexed [0 ..]
