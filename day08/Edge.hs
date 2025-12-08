9b4acf2331a899aad6708bb5304003e6f32a572a
Jetze Zoethout <jzoethout@sogyo.nl>
2025-12-08 18:46:24 +0100

    Day 08 complete with more terrible code :copyright:
    Write Everything Twice

--- day08/Edge.hs
+++ day08/Edge.hs
@@ -10,11 +10,11 @@ data Edge = Edge
   , to            :: Coordinate3D
   } deriving (Show)
 
-edge :: Coordinate3D -> Coordinate3D -> Edge
-edge coord1 coord2 = Edge {squaredLength = squaredDistance coord1 coord2, from = coord1, to = coord2}
+makeEdge :: Coordinate3D -> Coordinate3D -> Edge
+makeEdge coord1 coord2 = Edge {squaredLength = squaredDistance coord1 coord2, from = coord1, to = coord2}
 
 generateEdges :: [Coordinate3D] -> [Edge]
-generateEdges coords = map (uncurry edge) $ pairs coords
+generateEdges coords = map (uncurry makeEdge) $ pairs coords
   where
     pairs []     = []
     pairs (x:xs) = map (x, ) xs <> pairs xs
@@ -40,3 +40,25 @@ componentSizes coordinates edges steps =
                    (M.map updateComponent componentMap)
                    (M.delete targetComponent $ M.insert sourceComponent (sourceSize + targetSize) componentSizesMap)
                    remaining
+
+findFinalEdge :: [Coordinate3D] -> [Edge] -> Edge
+findFinalEdge coordinates = go (M.fromList $ zip coordinates [0 ..]) (M.fromList $ zip [0 ..] [1 | _ <- coordinates])
+  where
+    go :: Map Coordinate3D Int -> Map Int Int -> [Edge] -> Edge
+    go _ _ [] = error "out of edges"
+    go componentMap componentSizesMap (edge@Edge {..}:remaining) =
+      let sourceComponent = componentMap ! from
+          targetComponent = componentMap ! to
+          sourceSize = componentSizesMap ! sourceComponent
+          targetSize = componentSizesMap ! targetComponent
+          updateComponent comp =
+            if comp == targetComponent
+              then sourceComponent
+              else comp
+       in if sourceComponent == targetComponent
+            then go componentMap componentSizesMap remaining
+            else let updatedComponentSizes =
+                       M.delete targetComponent $ M.insert sourceComponent (sourceSize + targetSize) componentSizesMap
+                  in if M.size updatedComponentSizes == 1
+                       then edge
+                       else go (M.map updateComponent componentMap) updatedComponentSizes remaining
--- day08/Main.hs
+++ day08/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
-import           Coordinate3D  (parseCoordinate3D)
+import           Coordinate3D  (Coordinate3D (..), parseCoordinate3D)
 import           Data.Function (on)
 import           Data.List     (sort, sortBy)
 import qualified Data.Text     as T
-import           Edge          (Edge (squaredLength), componentSizes,
+import           Edge          (Edge (..), componentSizes, findFinalEdge,
                                 generateEdges)
 import           ProcessFile   (processFile)
 
@@ -14,3 +14,5 @@ main =
     let coordinates = map parseCoordinate3D $ T.lines text
         sortedEdges = sortBy (compare `on` squaredLength) $ generateEdges coordinates
     print $ product $ take 3 $ sortBy (flip compare) $ componentSizes coordinates sortedEdges 1000
+    let finalEdge = findFinalEdge coordinates sortedEdges
+    print $ finalEdge.from.x * finalEdge.to.x
